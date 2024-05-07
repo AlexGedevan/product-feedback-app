00bab6d1e4e7f45d0d9876213750cb52502ade71
BlindManCry <davit.avtandilashvili4@gmail.com>
2024-05-08 00:55:41 +0200

    make ui for createfeedback component (unfinished)

--- src/pages/CreateFeedback.jsx
+++ src/pages/CreateFeedback.jsx
@@ -17,13 +17,13 @@ function CreateFeedback() {
         <FeedbackCategory>
           <h2>Category</h2>
           <p>Choose a category for your feedback</p>
-          <select>
+          <Select>
             <option>Feature</option>
             <option>Enhancement</option>
             <option>UI</option>
             <option>UX</option>
             <option>Bug</option>
-          </select>
+          </Select>
         </FeedbackCategory>
         <FeedbackDescription>
           <h2>Feedback Detail</h2>
@@ -42,11 +42,13 @@ const StyledCreateFeedback = styled.div`
 `;
 
 const CreateFeedbackForm = styled.div`
-  background-color: white;
+  background-color: #ffffff;
   margin-top: 6.8rem;
-  padding-top: 5.2rem;
-  padding-left: 4.2rem;
-  padding-right: 4.2rem;
+  padding: 5.2rem 4.2rem 4rem;
+  border-radius: 10px;
+  display: flex;
+  flex-direction: column;
+  gap: 2.4rem;
   & > h1 {
     font-size: 2.4rem;
     font-weight: 700;
@@ -78,56 +80,71 @@ const FeedbackTitleHeadline = styled.div`
   flex-direction: column;
   gap: 2px;
   & > h2 {
-    font-size: 14px;
+    font-size: 1.4rem;
     font-weight: 700;
-    line-height: 20.23px;
+    line-height: 2.023rem;
     letter-spacing: -0.1944444477558136px;
-    text-align: left;
     color: #3a4374;
   }
   & > p {
-    font-size: 14px;
+    font-size: 1.4rem;
     font-weight: 400;
-    line-height: 20.23px;
-    text-align: left;
+    line-height: 2.023rem;
     color: #647196;
   }
 `;
 
 const FeedbackCategory = styled.div`
   & > h2 {
-    font-size: 14px;
+    font-size: 1.4rem;
     font-weight: 700;
-    line-height: 20.23px;
+    line-height: 2.023rem;
     letter-spacing: -0.1944444477558136px;
-    text-align: left;
     color: #3a4374;
   }
   & > p {
-    font-size: 14px;
+    font-size: 1.4rem;
     font-weight: 400;
-    line-height: 20.23px;
-    text-align: left;
+    line-height: 2.023rem;
     color: #647196;
-  }
-  & > select {
-    padding: 1.2rem 2.2rem 1.3rem 2.4rem;
-    width: 100%;
-    border-radius: 5px;
-    & > option {
-      font-size: 15px;
-      font-weight: 400;
-      line-height: 21.68px;
-      text-align: left;
-      color: #3a4374;
-    }
-
-    &:focus {
-      outline: 1px solid #4661e6;
-    }
   }
 `;
 
-const FeedbackDescription = styled.div``;
+const Select = styled.select`
+  padding: 1.2rem 2.2rem 1.3rem 2.4rem;
+  width: 100%;
+  border-radius: 5px;
+  margin-top: 1.6rem;
+  background: #f7f8fd;
+  border: none;
+  & > option {
+    font-size: 15px;
+    font-weight: 400;
+    line-height: 21.68px;
+    text-align: left;
+    color: #3a4374;
+  }
+
+  &:focus {
+    outline: 1px solid #4661e6;
+  }
+`;
+
+const FeedbackDescription = styled.div`
+  & > h2 {
+    font-size: 1.4rem;
+    font-weight: 700;
+    line-height: 2.023rem;
+    letter-spacing: -0.1944444477558136px;
+    color: #3a4374;
+  }
+
+  & > p {
+    font-size: 1.4rem;
+    font-weight: 400;
+    line-height: 2.023rem;
+    color: #647196;
+  }
+`;
 
 export default CreateFeedback;
